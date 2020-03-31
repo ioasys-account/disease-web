cf1cc1b082ac7e1eb6a73072deaf7abe6d0e524e
Paloma Barroso <palomapbarroso@gmail.com>
2020-03-31 15:10:21 -0300

    Add container default to application

--- src/components/presentation/Menu/Menu.jsx
+++ src/components/presentation/Menu/Menu.jsx
@@ -21,6 +21,7 @@ const Menu = () => {
   const materialStyles = makeStyles({
     root: {
       display: 'flex',
+      width: '100%',
       '& .MuiListItem-gutters': {
         fontSize: 14,
         paddingLeft: 28,
@@ -34,6 +35,13 @@ const Menu = () => {
           backgroundColor: 'rgba(204, 54, 50, 0.1)',
           color: '#CC3632',
         },
+      },
+      '& .MuiDrawer-docked': {
+        width: '100%',
+        backgroundColor: 'red',
+      },
+      '& .MuiDrawer-paper': {
+        width: '260px',
       },
     },
   })();
